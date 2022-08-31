4ae8e78aa3b602bf6f98d203d1bc60603e6d3184
Sarah Pearl <106031283+sarahpearl911@users.noreply.github.com>
2022-08-31 08:46:03 -0700

    Create Day1 Band Name Generator Code.py

---
+++ Day1 Band Name Generator Code.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator!")
+
+city = input("What is the name of the city you grew up in?\n")
+
+pet = input("What is your pet's name?\n")
+
+print("Your band name is " + city + " " + pet)
